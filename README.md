733d9d66969a87596e8ad581583a761d99dbb812
Ifara Joshua <farajayh@gmail.com>
2022-11-04 11:50:51 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # hng-task-csv
+# Description
+This script takes a csv file, generates a Chip-0007 JSON object for each line, and creates 
+an output file with the sha256 hash of the JSON object for each line added under the column HASH
+# Usage
+Navigate to the folder where you have the script and type the command below
+php hng-csv-task.php "file-path"
+Example: php hng-csv-task.php "./NFT Naming csv - Team Clutch.csv"
+# Input File Format
+The input file should be a comma separated value (CSV) file
+The header should not include the HASH entry
+The text in the fields should not include a comma, since it is a comma separated file
+You can use any other symbol inplace of a comma
+
+Sample input output files are provided. 
+Input file: NFT Naming csv - Team Clutch.csv
+Output file: NFT Naming csv - Team Clutch.output.csv
--- hng-csv-task.php
+++ hng-csv-task.php
@@ -1,38 +1,43 @@
 <?php
-    //$filename = "./NFT Naming csv - Team Clutch.csv";
     if($argc < 2){
         echo "Please specify a file name";
         die();
     }
+
     $filename = $argv[1];
     $filename_array = explode("/", $filename);
     $real_filename = array_pop($filename_array);
+
     $handle = @fopen($filename, "r");
-    $out_filename = "output-$real_filename";
+
+    $out_filename = str_replace('.csv', '', $real_filename).'.output'.'.csv';
+
     if(file_exists($out_filename)){
         unlink($out_filename);
     }
-    $out_handle = fopen($out_filename, 'a');
+
+    $out_handle = @fopen($out_filename, 'a');
+
     if ($handle) {
         $header = fgets($handle);
         $header_arr = explode(',', $header);
-        fwrite($out_handle, $header);
-        array_pop($header_arr);
-        //print_r($header_arr);
-        while (($line = fgets($handle)) !== false) {
-            // process the line read.
-            //echo $line."\n";
-            $line_arr = explode(',', $line);
-            array_pop($line_arr);
-            //print_r($line_arr);
-            $json_arr = array_combine($header_arr, $line_arr);
-            $json_hash = hash('sha256', json_encode($json_arr));
-            fwrite($out_handle, $line.",$json_hash");
-            break;
-            
+        
+        fwrite($out_handle, trim($header).",HASH\n");
+        
+        while (($line = fgets($handle)) !== false) {            
+            try {
+                $line_arr = explode(',', $line);
+                $json_arr = array_combine($header_arr, $line_arr);
+                $json_hash = hash('sha256', json_encode($json_arr));
+                fwrite($out_handle, trim($line).",$json_hash\n");
+            } catch (Error $e){
+                echo "Unexpected Error";
+                die();
+            }            
         }
     
         fclose($handle);
+        fclose($out_handle);
     } else {
         echo "Could not open file";
     }
